757c974b3933b9f839b3e91b079f66dda91af6af
Andrea <andiee.grr@gmail.com>
2023-01-20 19:52:03 -0600

    Update wonder model

--- M6-Android/Practica1/GreatWondersAPI/app/src/main/java/com/andreagr/greatwondersapi/GreatWonder.kt
+++ M6-Android/Practica1/GreatWondersAPI/app/src/main/java/com/andreagr/greatwondersapi/GreatWonder.kt
@@ -3,9 +3,18 @@ package com.andreagr.greatwondersapi
 data class GreatWonder(
     val name: String,
     val image: String,
+    val description: String,
+    val wonderLocation: WonderLocation
+)
+
+data class WonderLocation(
     val city: String,
     val country: String,
-    val description: String,
+    val flagImage: String,
+    val coords: Coords
+)
+
+data class Coords(
     val latitude: Double,
     val longitude: Double
 )
