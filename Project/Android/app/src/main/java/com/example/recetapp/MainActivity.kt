0182fba2259bd70b6a472524ce5ca465f5b90811
Andrea García <andiee.grr@gmail.com>
2023-03-08 01:34:55 -0600

    Implement Navigation Drawer

--- Project/Android/app/src/main/java/com/example/recetapp/MainActivity.kt
+++ Project/Android/app/src/main/java/com/example/recetapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.recetapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.core.view.GravityCompat
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.recetapp.databinding.ActivityMainBinding
@@ -12,6 +14,19 @@ import com.example.recetapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+            ?.findNavController()
+    }
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            ), binding.drawerLayout
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +34,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setSupportActionBar(binding.toolbar)
+        navController?.let {
+            setupActionBarWithNavController(it, appBarConfiguration)
+            with(binding) {
+                navView.setupWithNavController(it)
+                navDrawerView.setupWithNavController(it)
+            }
+        }
+
+        setupBackPressedCallback()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController?.navigateUp(appBarConfiguration) ?:  false || super.onSupportNavigateUp()
+    }
+
+    private fun setupBackPressedCallback() {
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    with(binding) {
+                        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                            drawerLayout.closeDrawer(GravityCompat.START)
+                        } else {
+                            onBackPressedDispatcher.onBackPressed()
+                        }
+                    }
+                }
+            }
+        )
     }
 }
