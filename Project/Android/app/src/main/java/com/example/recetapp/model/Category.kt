7d71eb03bcaec1bf0c7807bb715d69c412f5665d
Andrea García <andiee.grr@gmail.com>
2023-03-12 16:30:12 -0600

    Update project structure

--- Project/Android/app/src/main/java/com/example/recetapp/model/Category.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recetapp.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Category(
-    val name: String,
-    @SerializedName("image")
-    val imageUrl: String
-)
--- Project/Android/app/src/main/java/com/example/recetapp/model/CategorySelected.kt
+++
@@ -1,8 +0,0 @@
-package com.example.recetapp.model
-
-import com.example.recetapp.CategoryType
-
-data class CategorySelected(
-    val name: String,
-    val type: CategoryType
-)
