6723c86c44dbca9cf5ec106c0bfe7481fdfce449
Andrea García <andiee.grr@gmail.com>
2023-03-20 01:27:48 -0600

    Project structure change

--- Project/Android/app/src/main/java/com/example/recetapp/ui/UIResponseState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recetapp.ui
-
-sealed class UIResponseState {
-    object Loading: UIResponseState()
-    data class Error(val errorMessage: String): UIResponseState()
-    data class Success<T>(val content: T): UIResponseState()
-}
