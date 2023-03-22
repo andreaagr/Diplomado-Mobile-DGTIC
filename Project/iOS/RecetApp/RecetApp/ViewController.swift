3fdee303c57252157631350aae9f3e709435ccc0
Andrea García <andiee.grr@gmail.com>
2023-03-22 11:33:00 -0600

    Empty project

---
+++ Project/iOS/RecetApp/RecetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecetApp
+//
+//  Created by Andrea García Ruiz on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
